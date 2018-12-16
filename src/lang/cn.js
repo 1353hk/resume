796caeb3af609d6448360021709dc7e3cab11c42
KAZUKI <376073739@qq.com>
2018-12-16 17:02:09 +0800

    初始化

---
+++ src/lang/cn.js
@@ -0,0 +1,14 @@
+/* eslint unicode-bom: "off" */
+// Chinese (Simplified)
+const lang = {
+    born: '生日',
+    bornIn: '来自',
+    contact: '联系方式',
+    experience: '工作经历',
+    education: '教育经历',
+    skills: '技能专长',
+    projects: '项目',
+    contributions: '贡献',
+    about: '自我介绍'
+};
+export default lang;
